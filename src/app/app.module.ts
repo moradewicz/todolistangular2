2a2c11a934f58e10138582254aa27657bea59bb7
Moradewicz <moradewicz@hotmail.com>
2017-03-29 12:28:30 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: true })
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {CoursesComponent} from './courses.component';
+
+@Component({
+  selector: 'home',
+  styleUrls: ['./home.component.css'],
+  templateUrl: './home.component.html'
+})
+
+
+export class HomeComponent {
+
+}
